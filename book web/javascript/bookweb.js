39e3fd30d1633fcfb9c99d7e8e5ca72c20f1de7e
rahulvankar <vankarrahul2710@gmail.com>
2023-01-24 03:54:18 -0800

    adj

---
+++ book web/javascript/bookweb.js
@@ -0,0 +1,16 @@
+window.onscroll = () =>{
+    if(window.scroll > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+
+    }
+}
+window.onload = () =>{
+    if(window.scroll > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+
+    }
+}
